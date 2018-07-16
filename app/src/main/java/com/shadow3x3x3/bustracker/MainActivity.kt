01ff88adf0c8c748c008f03a74899dccf7744d1d
Jeff Chiu <shadow3x3x3@gmail.com>
2018-07-16 10:57:55 +0800

    Init Android Project

---
+++ app/src/main/java/com/shadow3x3x3/bustracker/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.shadow3x3x3.bustracker
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
