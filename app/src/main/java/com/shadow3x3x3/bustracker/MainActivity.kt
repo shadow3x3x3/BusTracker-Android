efd22dc293bde6f83f3efbddf2ab36a8a3e07f3f
Jeff Chiu <shadow3x3x3@gmail.com>
2018-07-19 14:11:14 +0800

    Disable view

--- app/src/main/java/com/shadow3x3x3/bustracker/MainActivity.kt
+++ app/src/main/java/com/shadow3x3x3/bustracker/MainActivity.kt
@@ -8,12 +8,10 @@ import com.shadow3x3x3.bustracker.service.LocationService
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
         val intent = Intent(this, LocationService::class.java)
         startService(intent)
+
+        finish()
+        super.onCreate(savedInstanceState)
     }
-
-
 }
